a45ae7e8c1fe7a6ad6aef459fa2358a1edd39d04
Sachin <sachinbajaj0001@gmail.com>
2024-05-31 23:45:17 +0530

    Fix clerk auth issue

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,9 @@
 import { authMiddleware } from "@clerk/nextjs";
- 
-export default authMiddleware({});
- 
+
+export default authMiddleware({
+  // publicRoutes: ["/"], // Include the root path as public
+});
+
 export const config = {
   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
- 
